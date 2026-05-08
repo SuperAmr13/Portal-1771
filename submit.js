318ca831c7264b05c89a3d2a751ac49099f833ce
SuperAmr13 <meromero132005@gmail.com>
2026-05-08 19:33:28 +0300

    Delete submit.js

--- submit.js
+++
@@ -1,7 +0,0 @@
-import { getSeat } from "../../lib/seats";
-
-export default function handler(req,res){
-  const data = req.body;
-  const seat = getSeat(data.power);
-  res.json({success:true, seat});
-}
